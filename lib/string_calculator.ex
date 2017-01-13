76e5570eee55c548c773faea7615c5ee02f5d3ba
Doug Morgan <dmorgan@onshift.com>
2017-01-13 11:57:10 -0500

    Next test done

--- lib/string_calculator.ex
+++ lib/string_calculator.ex
@@ -2,4 +2,10 @@ defmodule StringCalculator do
 
   def add(""), do: 0
 
+  def add(addends) do
+    addends
+    |> String.split(",")
+    |> Enum.map(&String.to_integer(&1))
+    |> Enum.reduce(0, &(&1 + &2))
+  end
 end
--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -5,4 +5,16 @@ defmodule ElixirKataTest do
   test "empty string returns zero" do
     assert StringCalculator.add("") == 0
   end
+
+  test "should add single number" do
+    assert StringCalculator.add("6") == 6
+  end
+  
+  test "should add two numbers" do
+    assert StringCalculator.add("7,5")== 12
+  end
+
+  test "should add three numbers" do 
+    assert StringCalculator.add("3,5,6") == 14
+  end 
 end
